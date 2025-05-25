deabb2ccd8b8c71a6ac401dcde9b08777b3ed18c
13octt <thirdteenth.octt@gmail.com>
2025-05-25 10:18:56 +0700

    render the rest of the form fields for the transaction form

--- components/ui/transaction-form.tsx
+++ components/ui/transaction-form.tsx
@@ -1,6 +1,7 @@
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
+import { format } from "date-fns"
 import {
     Form,
     FormControl,
@@ -10,12 +11,22 @@ import {
     FormMessage,
 } from "@/components/ui/form"
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import { CalendarIcon } from "lucide-react"
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/components/ui/popover"
+import { Calendar } from "@/components/ui/calendar"
+import { Input } from "./input"
 
 const transactionFormSchema = z.object({
     transactionType: z.enum(["income", "expense"]),
     categoryId: z.coerce.number().positive("Please select a category"),
     transactionDate: z.date().max(new Date()),
-    amount: z.coerce.number().positive("Amout must be greater than 0"),
+    amount: z.coerce.number().positive("Amount must be greater than 0"),
     description: z.string().min(3, "Description must contain at least 3 characters").max(300, "Description must contain a maximum of 300 characters")
 })
 
@@ -31,9 +42,13 @@ export function TransactionForm() {
         }
     })
 
+    function onSubmit(values: z.infer<typeof transactionFormSchema>) {
+        console.log(values)
+    }
+
     return (
         <Form {...form}>
-            <form >
+            <form onSubmit={form.handleSubmit(onSubmit)}>
                 <fieldset className="grid grid-cols-2 gap-y-5 gap-x-2">
                     <FormField
                         control={form.control}
@@ -57,7 +72,101 @@ export function TransactionForm() {
                             </FormItem>
                         )}
                     />
+
+                    <FormField
+                        control={form.control}
+                        name="categoryId"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Transaction Type</FormLabel>
+                                <FormControl>
+                                    <Select value={field.value.toString()} onValueChange={field.onChange} >
+                                        <SelectTrigger>
+                                            <SelectValue placeholder="Category" />
+                                        </SelectTrigger>
+                                        <SelectContent>
+
+                                        </SelectContent>
+                                    </Select>
+                                </FormControl>
+
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+
+                    <FormField
+                        control={form.control}
+                        name="transactionDate"
+                        render={({ field }) => (
+                            <FormItem className="flex flex-col">
+                                <FormLabel>Transaction Date</FormLabel>
+                                <FormControl>
+                                    <Popover>
+                                        <PopoverTrigger asChild>
+                                            <Button
+                                                variant={"outline"}
+                                                className={cn(
+                                                    "w-[full] justify-start text-left font-normal",
+                                                    !field.value && "text-muted-foreground"
+                                                )}
+                                            >
+                                                <CalendarIcon className="mr-2 h-4 w-4" />
+                                                {field.value ? format(field.value, "PPP") : <span>Pick a date</span>}
+                                            </Button>
+                                        </PopoverTrigger>
+                                        <PopoverContent className="w-auto p-0" align="start">
+                                            <Calendar
+                                                mode="single"
+                                                selected={field.value}
+                                                onSelect={field.onChange}
+                                                disabled={{
+                                                    after: new Date()
+                                                }}
+                                                initialFocus
+
+                                            />
+                                        </PopoverContent>
+                                    </Popover>
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+
+                    <FormField
+                        control={form.control}
+                        name="amount"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Amount</FormLabel>
+                                <FormControl>
+                                    <Input type="number" step={0.01} {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+
+
                 </fieldset>
+                <FormField
+                    control={form.control}
+                    name="description"
+                    render={({ field }) => (
+                        <FormItem className={cn("mt-4")}>
+                            <FormLabel>Description</FormLabel>
+                            <FormControl>
+                                <Input placeholder="Description" {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <Button
+                    type="submit"
+                    className="w-full mt-6"
+                >Submit</Button>
             </form>
         </Form>
     )
